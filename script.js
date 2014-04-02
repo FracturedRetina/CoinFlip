5b39e8ad64e7c605a5f48de50d5bf63d8a7bc206
Evan Shimoniak <eshimoniak@gmail.com>
2014-04-01 20:06:09 -0700

    update document.ready

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 var coin = loadCoin("res/us/quarter.json");
 
 $(document).ready(function() {
-	if ($(window).width() > $(window).height()) {
-		$('img#coin').height('75%');
-		$('img#coin').height($('img#coin').width());
-	} else {
-		$('img#coin').width('75%');
-		$('img#coin').width($('img#coin').height());
-	}
+	
 });
 
 $('img#coin').click(function() {
