b9e861ab2e73428e65329156d64c8be721507a55
Evan Shimoniak <eshimoniak@gmail.com>
2014-04-01 20:07:16 -0700

    Added console logs

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ $(document).ready(function() {
 $('img#coin').click(function() {
 	if (Math.random() < 0.5) {
 		$(this).attr('src', coin.headSrc);
+		console.log("Heads");
 	} else {
 		$(this).attr('src', coin.tailSrc);
+		console.log("Tails");
 	}
 });
 
