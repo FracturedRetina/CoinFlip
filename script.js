1aafbeddc03fd928b0e41aabe34e6798d261cccd
Evan Shimoniak <eshimoniak@gmail.com>
2014-04-02 20:52:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 var coin = loadCoin("res/us/quarter.json");
 
 $(document).ready(function() {
-	
+	if ($(window).width() > $(window).height()) {
+		$('img#coin').height('75%');
+		$('img#coin').height($('img#coin').width());
+	} else {
+		$('img#coin').width('75%');
+		$('img#coin').width($('img#coin').height());
+	}
 });
 
 $('img#coin').click(function() {
 	if (Math.random() < 0.5) {
 		$(this).attr('src', coin.headSrc);
-		console.log("Heads");
 	} else {
 		$(this).attr('src', coin.tailSrc);
-		console.log("Tails");
 	}
 });
 
 function loadCoin(url) {
-	return $.getJSON(url);
+	return $.getJSON(url, function(response){
+		JSON = response;
+		alert(JSON.property);
+	});
 }
